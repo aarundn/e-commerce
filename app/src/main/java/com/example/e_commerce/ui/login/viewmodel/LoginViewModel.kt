6ac49f5015bdfc51c027b1513388bcafb04910b9
MCD <debchouneharoun@gmail.com>
2024-10-17 14:06:59 +0100

    adding view model factory

--- app/src/main/java/com/example/e_commerce/ui/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/e_commerce/ui/login/viewmodel/LoginViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.e_commerce.ui.login.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.e_commerce.data.repository.user.UserPreferenceRepository
+import com.example.e_commerce.data.repository.user.UserRepositoryDataSourceImpl
+import com.example.e_commerce.ui.common.viewmodel.UserViewModel
 
 class LoginViewModel(
     private val userRepository: UserPreferenceRepository
@@ -9,4 +12,17 @@ class LoginViewModel(
 
     suspend fun isUserLoggedIn() = userRepository.isUserLoggedIn()
     suspend fun saveUserState(isLoggedIn: Boolean) = userRepository.saveUserState(isLoggedIn)
+}
+
+class LoginViewModelFactory(private val userRepository: UserRepositoryDataSourceImpl) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        // Ensure that the class being requested is the UserViewModel
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            // Return a new instance of UserViewModel with the userRepository passed in
+            @Suppress("UNCHECKED_CAST")
+            return LoginViewModel(userRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
