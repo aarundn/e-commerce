e9eedb3f56c2218d4a28c3d7852b15fba2af5fe5
MCD <debchouneharoun@gmail.com>
2024-10-20 21:39:29 +0100

    login with Google

--- app/src/main/java/com/example/e_commerce/ui/login/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/login/fragments/LoginFragment.kt
@@ -1,22 +1,36 @@
 package com.example.e_commerce.ui.login.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.example.e_commerce.BuildConfig
 import com.example.e_commerce.R
+
 import com.example.e_commerce.data.models.Resource
-import com.example.e_commerce.data.repository.auth.FireBaseAuthRepository
 import com.example.e_commerce.data.repository.auth.FireBaseAuthRepositoryImpl
 import com.example.e_commerce.data.repository.user.UserRepositoryDataSourceImpl
 import com.example.e_commerce.databinding.FragmentLoginBinding
 import com.example.e_commerce.ui.common.views.ProgressDialog
 import com.example.e_commerce.ui.login.viewmodel.LoginViewModel
-import kotlinx.coroutines.flow.count
+import com.example.e_commerce.ui.showSnakeBarError
+import com.example.e_commerce.utils.CrashlyticsUtils
+import com.example.e_commerce.utils.LoginException
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.coroutines.launch
 
 
@@ -49,17 +63,23 @@ class LoginFragment : Fragment() {
         initViewModel()
     }
 
+
     private fun initListeners() {
         binding.signInBtn.setOnClickListener{
             loginViewModel.login()
         }
+        binding.googleSigninBtn.setOnClickListener{
+            loginWithGoogle()
+        }
     }
+
+
 
     private fun initViewModel() {
         lifecycleScope.launch {
             loginViewModel.loginState.collect { state ->
-                Log.d(TAG, "initViewModel: $state")
-                state?.let {
+
+                state.let {
                     when (it) {
                         is Resource.Loading -> {
                             progressDialog.show()
@@ -72,9 +92,12 @@ class LoginFragment : Fragment() {
                         }
 
                         is Resource.Error -> {
+                            val msg = it.exception?.message ?: getString(R.string.try_again_later)
                             progressDialog.dismiss()
                             Toast.makeText(requireActivity(),
                                 it.exception?.message, Toast.LENGTH_SHORT).show()
+                            view?.showSnakeBarError(it.exception?.message ?: getString(R.string.try_again_later))
+                            logAuthIssueToCrashlytics(msg, "Email&Pass")
                         }
                     }
                 }
@@ -82,7 +105,66 @@ class LoginFragment : Fragment() {
             }
         }
     }
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private fun loginWithGoogle(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(BuildConfig.client_id)
+            .requestEmail()
+            .requestProfile()
+            .build()
 
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        googleSignInClient.signOut()
+        googleSignInLauncher.launch(googleSignInClient.signInIntent)
+    }
+    private val googleSignInLauncher = registerForActivityResult(
+    ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val data: Intent? = result.data
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handelTask(task)
+        } else {
+            view?.showSnakeBarError(getString(R.string.google_sign_in_was_canceled_or_failed))
+        }
+    }
+
+    private fun handelTask(task: Task<GoogleSignInAccount>) {
+        try {
+
+            val account = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account.idToken!!)
+
+        } catch (e: Exception) {
+            view?.showSnakeBarError(e.message ?: getString(R.string.try_again_later))
+            val msg = e.message ?: getString(R.string.try_again_later)
+            logAuthIssueToCrashlytics(msg, "Google")
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = FirebaseAuth.getInstance().currentUser
+
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+    private fun logAuthIssueToCrashlytics(msg: String, provider: String) {
+        CrashlyticsUtils.sendCustomLogToCrashlytics<LoginException>(
+            msg,
+            CrashlyticsUtils.LOGIN_KEY to msg,
+            CrashlyticsUtils.LOGIN_PROVIDER to provider,
+        )
+    }
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
