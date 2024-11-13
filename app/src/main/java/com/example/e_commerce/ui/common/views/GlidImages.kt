51745a3de68b5e047b8bdf8b04f56185ab26dc7d
MCD <debchouneharoun@gmail.com>
2024-11-13 15:00:36 +0100

    set countdown timer

--- app/src/main/java/com/example/e_commerce/ui/common/views/GlidImages.kt
+++ app/src/main/java/com/example/e_commerce/ui/common/views/GlidImages.kt
@@ -3,11 +3,13 @@ package com.example.e_commerce.ui.common.views
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, imageUrl: String?) {
     Glide.with(view.context)
         .load(imageUrl)
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
         .into(view)
 }
--- app/src/main/java/com/example/e_commerce/ui/home/adapter/SalesAdAdapter.kt
+++ app/src/main/java/com/example/e_commerce/ui/home/adapter/SalesAdAdapter.kt
@@ -5,16 +5,31 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.e_commerce.databinding.ItemSalesAdBinding
 import com.example.e_commerce.ui.home.model.SalesUiAdModel
+import com.example.e_commerce.utils.CountdownTimer
+import kotlinx.coroutines.cancel
 
 class SalesAdAdapter(
      private val salesAds: List<SalesUiAdModel>
 ) : RecyclerView.Adapter<SalesAdAdapter.SalesAdViewHolder>() {
 
+    val timerList = mutableMapOf<String, CountdownTimer>()
     inner class SalesAdViewHolder(private val binding: ItemSalesAdBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(salesAd: SalesUiAdModel) {
             binding.salesAd = salesAd
+            salesAd.endAt?.let {
+                timerList[salesAd.id ?: ""]?.cancel()
+                timerList.remove(salesAd.id ?: "")
+                val timer = CountdownTimer(it) { hours, minutes, seconds ->
+                    binding.hoursTextView.text = hours.toString()
+                    binding.minutesTextView.text = minutes.toString()
+                    binding.secondsTextView.text = seconds.toString()
+                }
+                timer.start()
+                timerList.put(salesAd.id ?: "", timer)
+            }
+
             binding.executePendingBindings()
         }
     }
@@ -29,4 +44,11 @@ class SalesAdAdapter(
     }
 
     override fun getItemCount(): Int = salesAds.size
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        timerList.forEach { (_, timer) ->
+            timer.cancel()
+        }
+    }
 }
